13245510b703412b531351cecf89e1fab09296e1
Yancee Villanueva <yanceevillanueva16@gmail.com>
2024-08-18 17:12:05 +0800

    Change socket url

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 
-export const socket = io("https://websocket-live-chat-server.vercel.app", {
+export const socket = io("https://websocket-server-dun.vercel.app", {
   withCredentials: true,
   // transports: ["websocket"],
 });
